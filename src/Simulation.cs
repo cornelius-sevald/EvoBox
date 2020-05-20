21a1b9712e915d4a9b4f9728c945e24f764ce471
Cornelius Sevald-Krause <cornelius.sevald@protonmail.com>
2020-05-20 03:24:23 +0200

    Give jumpmen a fair chance
    Jumpmen will spawn into a pre-nutritius environment.
    
    In reference to issue #32.
    Time taken: 20 m.

--- src/Simulation.cs
+++ src/Simulation.cs
@@ -4,6 +4,9 @@ namespace evobox {
 
     public sealed class Simulation {
 
+        // Time to 'warm up' the environment before spawning the jumpman.
+        const double WARMUP_TIME = 1;
+
         private static Random rand;
         private static Environment env;
         private static Minimap minimap;
@@ -27,6 +30,11 @@ namespace evobox {
 
             // Create the environment.
             env = new Environment(30, 30, new Random(rand.Next()));
+
+            // 'Warm up' the environment.
+            for (int i = 0; i < WARMUP_TIME * 60; i++) {
+                env.Update(1.0/6.0);
+            }
 
             // Add a jumpman to the environment with some energy.
             env.AddObject(
